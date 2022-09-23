ae8d596c5f9c200150ae2d5be0e9a54d0c57b9be
SamB2001 <sam.bischoff2001@gmail.com>
2022-09-22 19:19:11 -0600

    pokedex

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import Header from './components/Header'
+import TeamSection from './components/TeamSection';
+import PokedexSection from './components/PokedexSection';
+import axios from 'axios'
+import React, {useState, UseEffect, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [allPokemon, setAllPokemon] = useState([])
+  const [team, setTeam] = useState([])
+
+
+  useEffect(() => {
+    axios
+    .get('https://pokeapi.co/api/v2/pokemon?limit=100000')
+    .then ((res) => {
+      setAllPokemon(res.data.results)
+    })
+  }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <TeamSection team={team} />
+      <PokedexSection allPokemon={allPokemon} setTeam={setTeam}/>
     </div>
   );
 }
---
+++ src/components/PokedexCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import axios from 'axios'
+
+const PokedexCard = ({pokemon, index, setTeam}) =>{
+
+    const addToTeam = () => {
+        axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${pokemon.id}`)
+        .then((res) => {
+            setTeam((prevState) => {
+                if(prevState.length === 6) return prevState
+                let newState = [...prevState, res.data]
+                return newState
+            })
+        })
+    }
+
+    return (
+        <div className='pokedex-card'>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} className="pokemon-img" />
+            <h3>{pokemon.name}</h3>
+            <button onClick={addToTeam} >Add To Team</button>
+        </div>
+    )
+}
+
+export default PokedexCard
